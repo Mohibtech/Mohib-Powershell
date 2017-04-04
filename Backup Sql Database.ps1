1239492965c5654681e16867dba8aca35afded78
Mohib Alvi <mohibtech@gmail.com>
2017-04-04 11:36:52 +0500

    Update Backup Sql Database.ps1

--- Backup Sql Database.ps1
+++ Backup Sql Database.ps1
@@ -7,6 +7,10 @@ $ext = '.bak'
 $dbname = 'Sales_DW'
 $srvr = 'GT030\SQLEXPRESS'
 $baseDir='D:\Backup\SQLServer\SalesDW\'
-$backupDest = $baseDir +  dbname + $ext
+$backupDest = $baseDir +  $dbname + $ext
+
+$backupQuery = "BACKUP DATABASE $dbname TO DISK=N'$backupDest' WITH INIT"
 
 Backup-SqlDatabase -ServerInstance $srvr  -Database $dbname -BackupFile $backupDest
+
+#Invoke-Sqlcmd  -Query $backupQuery ;
