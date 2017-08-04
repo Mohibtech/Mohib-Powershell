f5cb82398820e7d006ecb5b14718ef34192bb356
Mohib Alvi <mohibtech@gmail.com>
2017-08-04 10:54:42 +0500

    Update Sqlplus Query in Powershell.ps1

--- Sqlplus Query in Powershell.ps1
+++ Sqlplus Query in Powershell.ps1
@@ -13,11 +13,3 @@ $outputfile = "sql_log.txt"
 $sqlQuery | sqlplus -silent $username/$password@$tnsalias | Out-File $outputfile 
 
 
-Sqlldr in Powershell
-
-$username = "U_83201254955235"
-$password = "U_83201254955235"
-$tnsalias = "orcl"
-$control="TKMX01V1_QC1_DC_EXT.ctl"
-$log="DC_EXT.log"
-$outputfile = "sql_log.txt" 
