82f86203c8627ae5e68f67d54a3181a6bd8f956a
Mohib Alvi <mohibtech@gmail.com>
2017-09-08 12:14:05 +0500

    Create Get-OraResultDataTable.ps1

---
+++ oracle/ManagedDriver/Get-OraResultDataTable.ps1
@@ -0,0 +1,54 @@
+function Get-OraResultDataTable{
+    [CmdLetBinding()]
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$conString,
+        [Parameter(Mandatory=$true)]
+        [string]$sqlString
+    )
+    begin {
+        $resultSet=@()
+    }
+    process {
+        try{
+            Write-Verbose ("Connection String: {0}" -f $conString)
+            Write-Verbose ("SQL Command: `r`n {0}" -f $sqlString)
+
+            Add-Type -Path "C:\SOFTWARE\Databases\Oracle\Oracle Windows\ODAC\ODAC 12c\odp.net\managed\common\Oracle.ManagedDataAccess.dll"
+            $con=New-Object Oracle.ManagedDataAccess.Client.OracleConnection($conString)
+            $cmd=$con.CreateCommand()
+            $cmd.CommandText=$sqlString
+
+            $da=New-Object Oracle.ManagedDataAccess.Client.OracleDataAdapter($cmd);
+
+            $resultSet=New-Object System.Data.DataTable
+
+            [void]$da.fill($resultSet)
+        }
+        catch{
+            Write-Error($_.Exception.ToString())
+        }
+        finally{
+            if($con.State-eq'Open'){ 
+                $con.close() 
+             }
+        }
+    }
+    end {
+        $resultSet
+    }
+
+}
+
+$userName = "system"
+$password = "password"
+$datasource = "srv/orcl"  # HOST/oracle service
+$connectionString="User Id=$username;Password=$password;Data Source=$datasource"
+
+$query=@"
+SELECT country_id, Country_name, Region_id
+FROM HR.COUNTRIES
+"@
+
+$results = Get-OraResultDataTable -conString $connectionString -sqlString $query -Verbose
+$results | SELECT country_id, Country_name, Region_id | Export-CSV "C:\test.csv" -NoTypeInformation
