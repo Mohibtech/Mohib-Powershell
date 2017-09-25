77fabd230014e6a9fcbe76ce07e23aa7e6b13abb
Mohib Alvi <mohibtech@gmail.com>
2017-09-25 10:53:41 +0500

    Update Short Scripts.ps1

--- short scripts/Short Scripts.ps1
+++ short scripts/Short Scripts.ps1
@@ -1,3 +1,16 @@
+# Multiple Short scripts
+
 
 Get-Process | Where-Object { $_.WS -ge 20MB }
+
+
+# Storing process result in variable using the variable parameter
+Get-Process notepad | Tee-Object -variable victims | Stop-Process
+$victims | select ProcessName,HasExited
+
+# Group Object by Status
+Get-Service | Group-Object Status
+
+# Group Object by Status and displaying Stopped Services
+(Get-Service | Group-Object Status)[0].Group
 
