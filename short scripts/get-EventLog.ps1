a5e890d404c0cfe024ed04996957be968b172065
Mohib Alvi <mohibtech@gmail.com>
2018-01-22 16:59:40 +0500

    Update and rename EventLog.ps1 to get-EventLog.ps1

--- short scripts/get-EventLog.ps1
+++ short scripts/get-EventLog.ps1
@@ -9,6 +9,10 @@ Get-EventLog -list | Where {$_.logdisplayname -eq "System"}
 # retrieves only those events in the Windows PowerShell event log that have an EventID equal to 403
 Get-EventLog "Windows PowerShell" | Where {$_.EventID -eq 403}
 
+# Equivalent of more command in linux
+get-eventlog -LogName "Windows PowerShell" | Out-Host -Paging
+
 # Find all sources that are represented in a specific number of entries in an event log
 $Events = Get-Eventlog -LogName system -Newest 1000
 $Events | Group-Object -Property source -noelement | Sort-Object -Property count -Descending
+
