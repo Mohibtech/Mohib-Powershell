8e3bb52ff2e31f0964c8a3ef399e983106f2bfc0
Mohib Alvi <mohibtech@gmail.com>
2019-03-03 23:22:39 +0500

    Update SetEnvironmentVar.ps1

--- Functions/SetEnvironmentVar.ps1
+++ Functions/SetEnvironmentVar.ps1
@@ -6,14 +6,21 @@
    Also sets general environment variables.
 .EXAMPLE
    SetEnvVariables -pathvar 'C:\WINDOWS\System32\OpenSSH\' -pathDel 'Yes'
-   Deletes path entry 'C:\WINDOWS\System32\OpenSSH\' from $PATH
+   Deletes path entry 'C:\WINDOWS\System32\OpenSSH\' from $PATH 
 .EXAMPLE
-   SetEnvVariables -pathvar 'C:\ProgramData\chocolatey\bin'
+   SetEnvVariables -pathvar ';C:\ProgramData\chocolatey\bin'
+   Need to add ';' in path string before adding path entry other it would concatenate with the last path entry and make it useless.
 .EXAMPLE
-   SetEnvVariables -EnvVarName 'Choco' -EnvVarValue 'C:\ProgramData\chocolatey\bin'
-
+   SetEnvVariables -EnvVarName 'SPARK_HOME' -EnvVarValue 'C:\spark\spark-2.4.0-bin-hadoop2.7'
+   This will add environment variable SPARK_HOME to system pointing to folder "C:\spark\spark-2.4.0-bin-hadoop2.7" 
 .INPUTS
-   Inputs to this cmdlet (if any)
+   -pathvar (new path entry, string starts with ';' for concatenation with other entries) 
+   -pathdel (provide 'Y' if want to delete a path entry in path variable, used with -pathvar argument)
+   -EnvVarName (name of new Environment Variable, use with -EnvVarValue)
+   -EnvVarValue (value of new Environment Variable, use with -EnvVarName)
+.NOTES
+   -pathvar and -pathdel are not used with last two arguments -EnvVarName and -EnvVarValue.
+   -EnvVarName and -EnvVarValue are used together as name and value pair.
 #>
 function SetEnvVariables {
     param(
